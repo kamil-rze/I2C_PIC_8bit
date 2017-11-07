c4f73313637b657555d9bcf6e390a47408af1d1c
kamil-rze <k.rzezwicki@gmail.com>
2017-11-07 10:31:34 +0000

    Create LM75.h

---
+++ LM75.h
@@ -0,0 +1,6 @@
+// Header file for LM75A - Digital Temperature Sensor and Thermal Watchdog with 2-Wire Interface
+
+#define TEMPERATURE     0x00
+#define CONFIGURATION   0x01
+#define T_HYST          0x02
+#define T_OS            0x03
